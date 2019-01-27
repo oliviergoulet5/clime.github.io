a3ddbb079311ce7c524e5f5eb3f4bb72ebe6251d
Olivier Goulet <oliviergoulet5@gmail.com>
2019-01-27 02:11:34 -0500

    Debugging
    Trying to get jQuery to function

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var weatherKey = "11b0138050052c987cb38016eabddacb";
 var weatherData;
-$getJSON("api.openweathermap.org/data/2.5/forecast/daily?q={toronto},{can}&cnt={7}", function(data) {
+jQuery.getJSON("api.openweathermap.org/data/2.5/forecast/daily?q={toronto},{can}&cnt={7}", function(data) {
     //data is the JSON string
     weatherData = data;
     console.log(weatherData);
