4ae83ccf5be727d915af6938b60e23e159b09bed
Olivier Goulet <oliviergoulet5@gmail.com>
2019-01-27 02:20:18 -0500

    More Debugging JQuery

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var weatherKey = "11b0138050052c987cb38016eabddacb";
 var weatherData;
-
-jquery.getJSON("api.openweathermap.org/data/2.5/forecast/daily?q={toronto},{can}&cnt={7}", function(data) {
+console.log(jquery);
+$.getJSON("api.openweathermap.org/data/2.5/forecast/daily?q={toronto},{can}&cnt={7}", function(data) {
     //data is the JSON string
     weatherData = data;
     console.log(weatherData);
