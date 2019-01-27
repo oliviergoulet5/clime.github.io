57fa1b4c9baf11f9ebef8fd417a14e4a704e0842
Olivier Goulet <oliviergoulet5@gmail.com>
2019-01-27 02:22:40 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 var weatherKey = "11b0138050052c987cb38016eabddacb";
 var weatherData;
-console.log(jquery);
 $.getJSON("api.openweathermap.org/data/2.5/forecast/daily?q={toronto},{can}&cnt={7}", function(data) {
     //data is the JSON string
     weatherData = data;
