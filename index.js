7ece139cf608a732c18c409e433041f5017f3dd2
Olivier Goulet <oliviergoulet5@gmail.com>
2019-01-28 13:17:46 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 var weatherKey = "11b0138050052c987cb38016eabddacb";
 var weatherData;
-$.getJSON("api.openweathermap.org/data/2.5/forecast/daily?q={toronto},{can}&cnt={7}", function(data) {
+$.getJSON("api.openweathermap.org/data/2.5/forecast/daily?" + weatherKey + "={toronto},{can}&cnt={7}", function(data) {
     //data is the JSON string
     weatherData = $.parseJSON(data);
     console.log(data);
 });
+
+//$.getJSON("api.openweathermap.org/data/2.5/forecast/daily?q={toronto},{can}&cnt={7}", function(data) {
