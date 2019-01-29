e95a29ddaa3ab31e6952cc27b95638ac15a80b41
Olivier Goulet <oliviergoulet5@gmail.com>
2019-01-29 11:20:14 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 // 5 Day Forecast
 var weatherKey = "11b0138050052c987cb38016eabddacb";
 var weatherData;
-$.getJSON("api.openweathermap.org/data/2.5/forecast?q=Toronto,us&mode=xml&APPID=" + weatherKey, function(data) {
+$.getJSON("api.openweathermap.org/data/2.5/forecast?q=Toronto,us&mode=json&APPID=" + weatherKey, function(data) {
     //data is the JSON string
     weatherData = $.parseJSON(data);
     console.log(data);
 });
+
+// find out today's day - label the headers correctly
