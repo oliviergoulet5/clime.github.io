26695a4b13ca528c7b11a57dd2bbcdb37fe43f6d
Olivier Goulet <oliviergoulet5@gmail.com>
2019-01-29 11:28:31 -0500

    Trial and Error API 404

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // 5 Day Forecast
 var weatherKey = "11b0138050052c987cb38016eabddacb";
 var weatherData;
-$.getJSON("api.openweathermap.org/data/2.5/forecast?q=Toronto,us&mode=json&APPID=" + weatherKey, function(data) {
+$.getJSON("https://api.openweathermap.org/data/2.5/forecast?q=Toronto,us&mode=json&APPID=" + weatherKey, function(data) {
     //data is the JSON string
     weatherData = $.parseJSON(data);
 });
