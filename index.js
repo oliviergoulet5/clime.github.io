ccd9f0b5c1ac9f0de8c1b1f8dd2ee4c1a11a0ea0
Olivier Goulet <oliviergoulet5@gmail.com>
2019-01-29 11:26:54 -0500

    API Update

--- index.js
+++ index.js
@@ -4,7 +4,8 @@ var weatherData;
 $.getJSON("api.openweathermap.org/data/2.5/forecast?q=Toronto,us&mode=json&APPID=" + weatherKey, function(data) {
     //data is the JSON string
     weatherData = $.parseJSON(data);
-    console.log(data);
 });
+
+console.log(weatherData);
 
 // find out today's day - label the headers correctly
