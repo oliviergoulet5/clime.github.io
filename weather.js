feadea99a34ce3b386b3dcc6d65f5f6d5d15123b
Olivier Goulet <oliviergoulet5@gmail.com>
2019-01-31 12:26:16 -0500

    Carding
    Converted the old labels into cards

--- weather.js
+++ weather.js
@@ -1,5 +1,30 @@
 // Get Date
 var days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+var today = {
+    dayIndex: new Date().getDay(),
+    dayName: days[new Date().getDay()]
+};
+
+$(document).ready(function(){
+    var cards = $("#cards-weather").children(".card");
+
+    cards.each(setupCards);
+});
+
+function setupCards(index, card){
+    console.log($(card).find(".card-title").innerHTML);
+    $(card).find(".card-title")[0].innerHTML = "--";
+    /*
+    for (var i=0; i<cards.length; i++){
+        var dayIndex = (today.dayIndex+i) % 7; // wraps around back to index 0 if value passes # of days
+        cards[i].innerHTML = days[dayIndex];
+    }*/
+}
+
+/*
+// Get Date
+var days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 var today = {
     dayIndex: new Date().getDay(),
     dayName: days[new Date().getDay()]
@@ -12,21 +37,14 @@ var labels = [
     document.getElementById("label-d"),
     document.getElementById("label-e")
 ];
-
-
-console.log(labels[today.dayIndex]);
-
-//document.getElementById("label-a").innerHTML = "AAAAAAAAAAAH";
-console.log(typeof today.dayIndex)
 // Change labels
-//var y = 0;
-
 for (var i=0; i<labels.length; i++){
     var dayIndex = (today.dayIndex+i) % 7; // wraps around back to index 0 if value passes # of days
     labels[i].innerHTML = days[dayIndex];
-    //going to break when date.dayIndex+i > 7
 }
+*/
 
+/*
 // 5 Day Forecast
 var weatherKey = "11b0138050052c987cb38016eabddacb";
 var weatherData;
@@ -34,4 +52,4 @@ var weatherData;
 $.getJSON("api.openweathermap.org/data/2.5/forecast?q=Toronto,us&mode=json&APPID=" + weatherKey, function(data) {
     //data is the JSON string
     weatherData = $.parseJSON(data);
-});
+});*/
