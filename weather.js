fd6b1926003c095b06db40e351e4f32d33185ab3
Olivier Goulet <oliviergoulet5@gmail.com>
2019-01-31 13:10:52 -0500

    Testing API

--- weather.js
+++ weather.js
@@ -1,3 +1,12 @@
+// API
+var weatherKey = "11b0138050052c987cb38016eabddacb";
+var weatherData;
+
+$.getJSON("api.openweathermap.org/data/2.5/forecast?q=Toronto,us&mode=json&APPID=" + weatherKey, function(data) {
+    //data is the JSON string
+    weatherData = $.parseJSON(data);
+});
+
 // Get Date
 var days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 
@@ -26,11 +35,4 @@ function setupCards(index, card){
 
 }
 
-// 5 Day Forecast
-var weatherKey = "11b0138050052c987cb38016eabddacb";
-var weatherData;
-
-$.getJSON("api.openweathermap.org/data/2.5/forecast?q=Toronto,us&mode=json&APPID=" + weatherKey, function(data) {
-    //data is the JSON string
-    weatherData = $.parseJSON(data);
-});
+console.log(weatherData);
